bc0df15855b694a37a78f239992cb88564416c7e
USMCsky <57950108+USMCsky@users.noreply.github.com>
2026-06-07 14:58:49 -0500

    Add Projectile class

---
+++ Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace rpg
+{
+    class Projectile
+    {
+        private Vector2 position;
+        private int speed = 1000;
+        public int radius = 18;
+        private Dir direction;
+
+        public Projectile(Vector2 newPos, Dir newDir)
+        {
+            position = newPos;
+            direction = newDir;
+        }
+        
+        public Vector2 Position { 
+            get { 
+                return position; 
+            } 
+        }
+
+        public void Update(GameTime gameTime) { 
+
+        
+        }
+    }
+}
